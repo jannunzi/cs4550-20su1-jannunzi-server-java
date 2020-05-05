1bbf59b5f721d6c7da51fa0b222a72e33b9bc5bf
jannunzi <jannunzi@gmail.com>
2020-05-05 13:48:29 -0400

    first commit

---
+++ src/test/java/com/example/cs455020su1jannunziserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs455020su1jannunziserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
